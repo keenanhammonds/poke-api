35a22c656bb9b399113a2abeaed1f0c806320f7c
keenanhammonds <keenan.hammonds@gmail.com>
2019-08-29 17:47:24 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const parser = require(body-parser);
+const app = express();
+const cors = require('cors');
+
+app.use(cors())
+app.use(parser.json())
+
+app.set("port", process.env.PORT || 8080)
+
+app.listen(app.get('port'), () => {
+    console.log(`PORT: ${app.get("port")}`)
+})
